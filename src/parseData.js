a68c67bcdb38e3ead80d7c31a59c0d2475c0ae52
Alex <88174852+umnovjp@users.noreply.github.com>
2021-10-21 22:12:06 -0600

    Add files via upload
    initial versions

--- src/parseData.js
+++ src/parseData.js
@@ -1,1 +1,5 @@
-
+const Manager= require("../lib/Manager");
+const getManager = indexData => {
+let newManager = new Manager(indexData.managerName, indexData.managerID, indexData.managerEmail, indexData.officeNumber);}
+    
+module.exports = getManager;
