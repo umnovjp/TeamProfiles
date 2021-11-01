f7bb56b8cdef4038c567ae556fbd5953e70561dd
Alex <88174852+umnovjp@users.noreply.github.com>
2021-10-31 18:58:40 -0600

    Delete parseData.js
    uploaded different file which actually works... finally

--- src/parseData.js
+++
@@ -1,8 +0,0 @@
-// const Employee = require("../lib/Employee");
-let Manager= require("../lib/Manager");
-const getManager = teamData => {
-// let newEmployee = new Employee() 
-// let newManager = new Manager(teamData.managerName, teamData.managerID, teamData.managerEmail, teamData.officeNumber);
-return `xxx yyy`}
-    
-module.exports = getManager;
