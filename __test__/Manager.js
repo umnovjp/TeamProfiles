998c34c02da2bbe46dd0feb8a3824171327ce6ef
Alex <88174852+umnovjp@users.noreply.github.com>
2021-10-31 21:06:33 -0600

    Delete Manager.js
    added wrong files

--- __test__/Manager.js
+++
@@ -1,8 +0,0 @@
-const Employee = require ('./Employee');
-class Manager extends Employee {constructor(officeNumber)
-{super(name, id, email);
-this.officeNumber = officeNumber;}
-getRole(i){const role = 'Manager'}//return 'Manager'
-getOfficeNumber(i){const officeNumber = data[i].officeNumber}
-}
-module.exports = Manager;
